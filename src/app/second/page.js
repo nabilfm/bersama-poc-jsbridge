eda2738a56feeb39f3ff3f3bc7d67dcbbcdb1c0b
nabilfm <muhammad.f@bankfama.co.id>
2023-03-30 16:55:52 +0700

    adjust logic

--- src/app/second/page.js
+++ src/app/second/page.js
@@ -2,11 +2,14 @@
 import { Inter } from 'next/font/google'
 import styles from '../page.module.css'
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Second() {
+  const [image, setImage] = useState("/next.svg")
   const [message, setMessage] = useState("")
+  const router = useRouter()
 
   useEffect(() => {
     window.onReceiveBackEvent = () => {
@@ -25,18 +28,36 @@ export default function Second() {
       </div>
       }
 
+      <div className={styles.center}>
+        <img
+         src={image} className={styles.logo} />
+      </div>
+
       <div className={styles.grid}>
       <a
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className} onClick={() => callJSBridgeToggleNavigationVisibility(title)}>
+          <h2 className={inter.className} >
             this is second page
           </h2>
           <p className={inter.className}>
-            native will call onReceiveBackEvent
+            native will detect back button pressed and call onReceiveBackEvent (js function)
           </p>
+        </a>
+      </div>
+      
+
+      <div className={styles.grid} onClick={() => router.back()}>
+      <a
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className} >
+            back to first page again  <span>-&gt;</span>
+          </h2>
         </a>
       </div>
     </main>
